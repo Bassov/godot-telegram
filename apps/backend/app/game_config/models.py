00d3d89fb1f7f0a94371c25e49a7f3ebb2da047a
Aleksandr Basov <bainst4@gmail.com>
2024-12-23 12:30:06 +0700

    add items config example

--- apps/backend/app/game_config/models.py
+++ apps/backend/app/game_config/models.py
@@ -11,12 +11,20 @@ class Level(SheetsModel):
     soft_currency_reward: int
     hard_currency_reward: int
 
+class Item(SheetsModel):
+    SHEET_NAME: ClassVar[str] = "items"
+
+    id: int
+    name: str
+    description: str
 
 class GameConfigs(BaseModel):
     levels: List[Level]
+    items: List[Item]
 
     @classmethod
     async def load_from_sheets(cls):
         return cls(
             levels=await Level.load_from_sheets(),
+            items=await Item.load_from_sheets(),
         )
